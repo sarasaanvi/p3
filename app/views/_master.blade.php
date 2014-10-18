9e98ec322b78da2fe93aa35b1c2a959403dfb5d8
unknown <hemadris@USMUYN0H455703.na02.lucent.com>
2014-10-18 22:27:11 +0200

    Adding css

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>
+	<link rel="stylesheet" type = "text/css" href=<?php public_path();?>"/style_hemadri.css">	
+	<title>		
 		@yield('title',"Developer's Best Friend")
 	</title>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
 	<link rel="stylesheet" type = "text/css" href="style_hemadri.css">	
 </head>
 <body>
-	@yield('content')
+	<div id = "container">
+		@yield('content')
+	</div>
 </body>
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,17 +5,20 @@
 	<br>
 	<h2>Lorem Ipsum Generator</h2>
 	<p>	
-	In publishing and graphic design, lorem ipsum is a place holder text commonly used to demonstrate the graphic elements of a document or visual presentation. By replacing the distraction of meaningful content with filler text of scrambled Latin it allows viewers to focus on graphical elements such as font, typography, and layout.
-	</p>	
+		In publishing and graphic design, lorem ipsum is a place holder text commonly used to demonstrate the graphic elements of a document or visual presentation. By replacing the distraction of meaningful content with filler text of scrambled Latin it allows viewers to focus on graphical elements such as font, typography, and layout.
+	</p>
+	<br>
 	Please enter the no of paragraphs to be generated (Max 99)
 	<form method="POST" action="/para">
 		<input type ="text"  name ="paras" >
 		<input type ="submit" value="Generate" >
 	</form>
+	<br>
 	<h2>Random User Generator</h2>
 	<p>	
-	Create random user data for your applications. Like Lorem Ipsum, but for people.
+		Create random user data for your applications. Like Lorem Ipsum, but for people.
 	</p>
+	<br>
 	Please enter the no of users to be generated (Max 99)
 	<form method="post" action="/user">
 		<input type ="text" name = "users" >
